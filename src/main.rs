22dd8e4e84df148d00ff8562e55eb235a9fd113f
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-07-14 18:22:59 +0900

    add file arguement

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ mod ir;
 mod lexer;
 mod parser;
 
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser)]
 struct Arg {
     #[arg(short)]
-    script: String,
+    script: Option<String>,
+    #[arg(short)]
+    file: Option<PathBuf>,
 }
 
 fn main() {
     let arg = Arg::parse();
-    let script = arg.script;
+    let script = match (arg.script, arg.file) {
+        (Some(script), None) => script,
+        (None, Some(path)) => std::fs::read_to_string(path).unwrap(),
+        (Some(_), Some(_)) | (None, None) => panic!("please give me arguement file or script"),
+    };
     let token = dbg!(lexer::lexer(&script)).unwrap();
     let ast = dbg!(parser::parse(&token)).unwrap();
     let ir = dbg!(ir::genir(ast)).unwrap();
